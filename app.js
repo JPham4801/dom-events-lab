ff88b0fd231182d9cf083280731c6ddf0ab77499
JPham4801 <JPham4801@gmail.com>
2024-06-16 17:23:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll(".button");
+const calculator = document.querySelector("#calculator");
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+calculator.addEventListener("click", (event) => {
+  // This log is for testing purposes to verify we're getting the correct value
+  // You have to click a button to see this log
+  console.log(event.target.innerText);
+
+  // Example
+  if (event.target.classList.contains("number")) {
+    // Do something with a number
+  }
+
+  // Example
+  if (event.target.innerText === "*") {
+    // Do something with this operator
+  }
+});
+
+/*-------------------------------- Functions --------------------------------*/
